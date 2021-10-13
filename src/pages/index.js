0b91620b86fcbc547fce1f7ac404899a88bb8b24
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-10-13 05:36:14 -0300

    Update project to Next.js and install material ui

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+export default function Home() {
+  return (
+    <div>
+      <h1>Front PL2</h1>
+      <Button variant="contained" color="secondary">
+        Hello World
+      </Button>
+    </div>
+  )
+}

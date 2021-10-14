466e9cdac8afda40f43c1f23c4862d3f0332d194
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-10-14 03:05:28 -0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 13,
+        "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
@@ -23,6 +23,14 @@ module.exports = {
     ],
     "rules": {
         "prettier/prettier": "error",
-        "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"]}]
+        "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"]}],
+        "react/prop-types": "off",
+        "react/jsx-props-no-spreading": ["error", {
+            "html": "ignore",
+            "custom": "ignore",
+            "exceptions": [""]
+          }],
+        "react/react-in-jsx-scope": "off",
+        "import/no-unresolved": "off"
     }
 };

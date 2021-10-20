b8ee2d61cbe1fa37c426452200ba601207382816
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-10-20 03:19:56 -0300

    Create mock file to ies filters

---
+++ src/mock/filters.js
@@ -0,0 +1,30 @@
+export const iesOptions = [
+  {
+    value: 1, label: 'Organização Acadêmica',
+    options: [
+      { value: 1,	label: "Universidade" },
+      { value: 2,	label: "Centro Universitário" },
+      { value: 3,	label: "Faculdade" },
+      { value: 4,	label: "Instituto Federal de Educação, Ciência e Tecnologia" },
+      { value: 5,	label: "Centro Federal de Educação Tecnológica" },
+    ]
+  },
+  {
+    value: 2, label: 'Categoria Administrativa',
+    options: [
+      { value: 1, label: "Pública Federal" },
+      { value: 2, label: "Pública Estadual" },
+      { value: 3, label: "Pública Municipal" },
+      { value: 4, label: "Privada com fins lucrativos" },
+      { value: 5, label: "Privada sem fins lucrativos" },
+      { value: 6, label: "Privada - Particular em sentido estrito" },
+      { value: 7, label: "Especial" },
+      { value: 8, label: "Privada comunitária" },
+      { value: 9, label: "Privada confessional" },
+    ]
+  },
+  {value: 3, label: 'Nome da Instituição',}
+
+];
+
+

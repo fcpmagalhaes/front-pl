94448fe72a8a60d697e6f92f7b782bd5eb32575f
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-02 21:26:03 -0300

    add new methods to control steps range year and ies

--- src/store/infographic/actions.js
+++ src/store/infographic/actions.js
@@ -1,47 +1,22 @@
 export const Types = {
-  // CLEAN_FORM_REPORT: 'dpr/CLEAN_FORM_REPORT',
-
-  // LOAD_PROGRAMS: 'dpr/LOAD_PROGRAMS',
-
-  // SAVE_REPORT: 'dpr/SAVE_REPORT',
-
-  // LOAD_COMPLETE: 'dpr/LOAD_COMPLETE',
-  // LOAD_ERROR: 'dpr/LOAD_ERROR',
-  
   LOAD_IES: 'infographic/LOAD_IES',
+  UPDATE_STEP: 'infographic/UPDATE_STEP',
+  SET_RANGE_YEARS: 'infographic/SET_RANGE_YEARS',
+  SET_IES_NAMES: 'infographic/SET_IES_NAMES',
 };
 
 // Action Creators
 export const Creators = {
-  // cleanReportForm: () => ({ type: Types.CLEAN_FORM_REPORT }),
-
-  // cleanProgramsList: () => ({ type: Types.CLEAN_PROGRAMS_LIST }),
-
-  // loadReport: (paginationParams, filterParams) => ({
-  //   type: Types.LOAD,
-  //   loading: true,
-  //   paginationParams,
-  //   filterParams,
-  // }),
-
-  // cleanErrorState: () => ({ type: Types.CLEAN_ERROR_STATE }),
-
-  // loadProgramProjects: (reportParams) => {
-  //   return {
-  //     type: Types.LOAD_PROJECTS,
-  //     payload: reportParams,
-  //   };
-  // },
-
-  // loadPrograms: () => ({ type: Types.LOAD_PROGRAMS }),
-
-  // loadProgram: ({ programId }) => ({
-  //   type: Types.LOAD_PROGRAM,
-  //   payload: { programId },
-  // }),
-
-  loadIes: (years) => ({
+  updateStep: (step) => ({
+    type: Types.UPDATE_STEP,
+    payload: step,
+  }),
+  setRangeYears: (range) => ({
+    type: Types.SET_RANGE_YEARS,
+    payload: range,
+  }),
+  loadIes: (rangeYears) => ({
     type: Types.LOAD_IES,
-    payload: years,
+    payload: rangeYears,
   })
 };

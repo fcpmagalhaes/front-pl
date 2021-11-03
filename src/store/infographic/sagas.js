6565b5c1d5ace5e9b2198823c495acf4c7e1eeab
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-03 06:29:40 -0300

    Create sagas to load college names and load research

--- src/store/infographic/sagas.js
+++ src/store/infographic/sagas.js
@@ -4,6 +4,12 @@ import { api } from '../../services/axios/index';
 import { Types } from './actions';
 
 
+async function getResearchResult(values) {
+  return api.post('/research', {
+    data: values,
+  });
+}
+
 async function getIesByYears(values) {
   const payload = {
     range: values,
@@ -14,17 +20,27 @@ async function getIesByYears(values) {
   });
 }
 
-function standardizeKeyValue(arrayObject) {
+async function getCollegeByYears(values) {
+  const payload = {
+    range: values,
+  };
+
+  return api.post('/college_names', {
+    data: payload,
+  });
+}
+
+function standardizeKeyValue(arrayObject, code, name) {
   const renamed = [];
   arrayObject.map( item => {
     renamed.push(
       _.mapKeys( item, ( value, key ) => {
           let newKey = key;
-          if( key === 'co_ies' ) {
+          if( key === code ) {
               newKey = 'value';
           }
 
-          if( key === 'no_ies' ) {
+          if( key === name ) {
               newKey = 'label';
           }
           return newKey;
@@ -40,7 +56,36 @@ function* loadIes(values) {
     yield put({
       type: Types.SET_IES_NAMES,
       payload: {
-        iesNames: standardizeKeyValue(response.data),
+        iesNames: standardizeKeyValue(response.data, 'co_ies', 'no_ies'),
+      },
+    });
+  } catch (err) {
+    yield put({ type: Types.LOAD_ERROR });
+  }
+}
+
+function* loadCollege(values) {
+  try {
+    const response = yield call(getCollegeByYears, values.payload);
+    yield put({
+      type: Types.SET_COLLEGE_NAMES,
+      payload: {
+        collegeNames: standardizeKeyValue(response.data, 'co_curso', 'no_curso'),
+      },
+    });
+  } catch (err) {
+    yield put({ type: Types.LOAD_ERROR });
+  }
+}
+
+function* loadResearch(values) {
+  try {
+    const response = yield call(getResearchResult, values.payload);
+    console.log('loadResearch', response);
+    yield put({
+      type: Types.SET_RESEARCH,
+      payload: {
+        researchData: response.data,
       },
     });
   } catch (err) {
@@ -50,4 +95,6 @@ function* loadIes(values) {
 
 export function* infographicSagas() {
   yield takeLatest(Types.LOAD_IES, loadIes);
+  yield takeLatest(Types.LOAD_COLLEGE, loadCollege);
+  yield takeLatest(Types.LOAD_RESEARCH, loadResearch);
 }

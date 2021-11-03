0f610d09c8efa7b09484c08a3ea564422303e2a9
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-02 21:25:22 -0300

    Add new reducers to control range year and ies names

--- src/store/infographic/reducer.js
+++ src/store/infographic/reducer.js
@@ -1,22 +1,48 @@
 import { Types } from './actions';
 
 const INITIAL_STATE = {
-  data: [],
+  dataFilters: [],
   loading: false,
   success: false,
   error: false,
+  activeStep: 0,
+  rangeYears: [],
+  iesNames: [],
 };
 
 // Reducer
 export default function infographic(state = INITIAL_STATE, action) {
+  // console.log('payload', action.payload);
+  console.log('state', state);
   switch (action.type) {
-    case Types.CLEAN_PROGRAMS_LIST:
+    case Types.UPDATE_STEP:
+      return {
+        ...state,
+        activeStep: action.payload,
+      };
+    case Types.SET_RANGE_YEARS:
+      return {
+        ...state,
+        rangeYears: action.payload,
+      };
+    case Types.LOAD_IES:
+      return {
+        ...state,
+        loading: true,
+        error: false
+      };
+    case Types.SET_IES_NAMES:
       return {
         ...state,
         loading: false,
-        loadingProgramList: false,
-        programsList: [],
-        program: null,
+        error: false,
+        iesNames: action.payload.iesNames
+      };
+    case Types.LOAD_ERROR:
+      return {
+        ...state,
+        error: true,
+        loading: false,
       };
 
     default:

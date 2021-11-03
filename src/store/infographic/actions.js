af7ba9abf0ff9a079d572e47524c8f8cd2579c14
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-03 06:31:01 -0300

    Create new functions to ies, college, student filters and final research

--- src/store/infographic/actions.js
+++ src/store/infographic/actions.js
@@ -1,8 +1,18 @@
 export const Types = {
+  LOAD_ERROR: 'infographic/LOAD_ERROR',
   LOAD_IES: 'infographic/LOAD_IES',
-  UPDATE_STEP: 'infographic/UPDATE_STEP',
+  LOAD_COLLEGE: 'infographic/LOAD_COLLEGE',
+  LOAD_RESEARCH: 'infographic/LOAD_RESEARCH',
+
   SET_RANGE_YEARS: 'infographic/SET_RANGE_YEARS',
   SET_IES_NAMES: 'infographic/SET_IES_NAMES',
+  SET_IES_FILTERS: 'infographic/SET_IES_FILTERS',
+  SET_COLLEGE_NAMES: 'infographic/SET_COLLEGE_NAMES',
+  SET_COLLEGE_FILTERS: 'infographic/SET_COLLEGE_FILTERS',
+  SET_STUDENT_FILTERS: 'infographic/SET_STUDENT_FILTERS',
+  SET_RESEARCH: 'infographic/SET_RESEARCH',
+
+  UPDATE_STEP: 'infographic/UPDATE_STEP',
 };
 
 // Action Creators
@@ -18,5 +28,26 @@ export const Creators = {
   loadIes: (rangeYears) => ({
     type: Types.LOAD_IES,
     payload: rangeYears,
-  })
+  }),
+  setIesFilters: (refinedFilters) => ({
+    type: Types.SET_IES_FILTERS,
+    payload: refinedFilters,
+  }),
+  loadCollege: (rangeYears) => ({
+    type: Types.LOAD_COLLEGE,
+    payload: rangeYears,
+  }),
+  setCollegeFilters: (refinedFilters) => ({
+    type: Types.SET_COLLEGE_FILTERS,
+    payload: refinedFilters,
+  }),
+  setStudentFilters: (refinedFilters) => ({
+    type: Types.SET_STUDENT_FILTERS,
+    payload: refinedFilters,
+  }),
+  loadResearch: (filters) => ({
+    type: Types.LOAD_RESEARCH,
+    payload: filters,
+  }),
+
 };
--- src/store/infographic/reducer.js
+++ src/store/infographic/reducer.js
@@ -1,19 +1,23 @@
 import { Types } from './actions';
 
 const INITIAL_STATE = {
-  dataFilters: [],
   loading: false,
   success: false,
   error: false,
   activeStep: 0,
   rangeYears: [],
   iesNames: [],
+  iesFilters: [],
+  collegeNames: [],
+  collegeFilters: [],
+  studentFilters: [],
+  researchData: [],
 };
 
 // Reducer
 export default function infographic(state = INITIAL_STATE, action) {
   // console.log('payload', action.payload);
-  console.log('state', state);
+  // console.log('state', state);
   switch (action.type) {
     case Types.UPDATE_STEP:
       return {
@@ -38,6 +42,51 @@ export default function infographic(state = INITIAL_STATE, action) {
         error: false,
         iesNames: action.payload.iesNames
       };
+    case Types.SET_IES_FILTERS:
+      return {
+        ...state,
+        iesFilters: action.payload
+      };
+    case Types.LOAD_COLLEGE:
+      return {
+        ...state,
+        loading: true,
+        error: false
+      };
+    case Types.SET_COLLEGE_NAMES:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        collegeNames: action.payload.collegeNames
+      };
+    case Types.SET_COLLEGE_FILTERS:
+      return {
+        ...state,
+        collegeFilters: action.payload
+      };
+    case Types.SET_STUDENT_FILTERS:
+      return {
+        ...state,
+        studentFilters: action.payload
+      };
+    case Types.LOAD_RESEARCH:
+      return {
+        ...state,
+        loading: true,
+        error: false
+      };
+    case Types.SET_RESEARCH:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        researchData: action.payload.researchData
+      };
+
+
+
+
     case Types.LOAD_ERROR:
       return {
         ...state,

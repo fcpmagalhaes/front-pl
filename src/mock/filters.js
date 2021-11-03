25365e01bff9819a78c01599731e6fc2231634e7
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2021-11-03 06:28:32 -0300

    Update mock datas

--- src/mock/filters.js
+++ src/mock/filters.js
@@ -1,37 +1,129 @@
-export const iesOptions = [
+export const studentOptionsMock = [
   {
-    value: 1, label: 'Organização Acadêmica', type: 'select',
+    value: 1, label: 'Cor Raça', type: 'select',
     options: [
-      { value: 1,	label: "Universidade" },
-      { value: 2,	label: "Centro Universitário" },
-      { value: 3,	label: "Faculdade" },
-      { value: 4,	label: "Instituto Federal de Educação, Ciência e Tecnologia" },
-      { value: 5,	label: "Centro Federal de Educação Tecnológica" },
+      { value: 0, label: "Aluno não quis declarar cor/raça" },
+      { value: 1, label: "Branca" },
+      { value: 2, label: "Preta" },
+      { value: 3, label: "Parda" },
+      { value: 4, label: "Amarela" },
+      { value: 5, label: "Indígena" },
+      { value: 9, label: "Não dispõe da informação (Não resposta)" },
     ]
   },
   {
-    value: 2, label: 'Categoria Administrativa', type: 'select',
+    value: 2, label: 'Sexo', type: 'select',
     options: [
-      { value: 1, label: "Pública Federal" },
-      { value: 2, label: "Pública Estadual" },
-      { value: 3, label: "Pública Municipal" },
-      { value: 4, label: "Privada com fins lucrativos" },
-      { value: 5, label: "Privada sem fins lucrativos" },
-      { value: 6, label: "Privada - Particular em sentido estrito" },
-      { value: 7, label: "Especial" },
-      { value: 8, label: "Privada comunitária" },
-      { value: 9, label: "Privada confessional" },
+      { value: 1, label: "Feminino" },
+      { value: 2, label: "Masculino" },
     ]
   },
-  {value: 3, label: 'Nome da Instituição', type: 'check'},
-  {value: 4, label: 'teste asdfasfdadsfa', type: 'check'},
-  {value: 5, label: '1231tituição', type: 'check'},
-  {value: 6, label: 'Nomeacasdfad sfadsfstituição', type: 'check'},
-  {value: 7, label: 'Nome da Insasdfo', type: 'check'},
-  {value: 8, label: 'Nome da Instituasdfas', type: 'check'},
+  {value: 3, label: 'Idade', type: 'input'},
+  {
+    value: 4, label: 'Tipo de Situação', type: 'select',
+    options: [
+      { value: 2, label:	"Cursando"},
+      { value: 3, label:	"Matrícula trancada"},
+      { value: 4, label:	"Desvinculado do curso"},
+      { value: 5, label:	"Transferido para outro curso da mesma IES"},
+      { value: 6, label:	"Formado"},
+      { value: 7, label:	"Falecido"},
+    ]
+  },
+  {
+    value: 5, label: 'Possui Deficiência', type: 'select',
+    options: [
+      { value: 0, label: "Não" },
+      { value: 1, label: "Sim" },
+      { value: 2, label: "Não Dispõe de Informação" },
+    ]
+  },
+  { value: 6, label: 'Ingresso Por Vestibular', type: 'check'},
+  { value: 7, label: 'Ingresso Por ENEM', type: 'check'},
+  { value: 8, label: 'Ingresso Por Avaliação Seriada', type: 'check'},
+  { value: 9, label: 'Ingresso Por Seleção Simplificada', type: 'check'},
+  { value: 10, label: 'Ingresso Por Outro Tipo de Seleção', type: 'check'},
+  { value: 11, label: 'Ingresso Por Vaga Remanescente', type: 'check'},
+  { value: 12, label: 'Ingresso Por Vaga Programa Especial', type: 'check'},
+  { value: 13, label: 'Ingresso Por Transf Exofficio', type: 'check'},
+  { value: 14, label: 'Ingresso Por Decisão Judicial', type: 'check'},
+  { value: 15, label: 'Ingresso Por Convênio PECG', type: 'check'},
+  { value: 16, label: 'Ingresso Por Egresso', type: 'check'},
+  { value: 17, label: 'Ingresso Por Outra Forma', type: 'check'},
+  { value: 18, label: 'Ingresso Por Reserva Vagas', type: 'check'},
+  { value: 19, label: 'Ingresso Por Reserva Etnico', type: 'check'},
+  { value: 20, label: 'Ingresso Por Reserva Deficiência', type: 'check'},
+  { value: 21, label: 'Ingresso Por Reserva Ens Público', type: 'check'},
+  { value: 22, label: 'Ingresso Por Reserva Renda Familiar', type: 'check'},
+  { value: 23, label: 'Ingresso Por Reserva Outra', type: 'check'},
+  { value: 24, label: 'Utiliza Financiamento Estudantil', type: 'check'},
+  { value: 25, label: 'Recebe Apoio Social', type: 'check'},
+  { value: 26, label: 'Participa de Atividade Extracurricular', type: 'check'},
+  {
+    value: 27, label: 'Escola Conclusão Ens. Médio', type: 'select',
+    options: [
+      { value: 1, label: "Pública" },
+      { value: 2, label: "Privada" },
+      { value: 9, label: "Não Dispõe de Informação" },
+    ]
+  },
+  {
+    value: 28, label: 'Semestre de Conclusão', type: 'select',
+    options: [
+      { value: 1, label: "Primeiro Semestre" },
+      { value: 2, label: "Segundo Semestre" },
+    ]
+  },
+  {
+    value: 29, label: 'Mobilidade Acadêmica', type: 'select',
+    options: [
+      { value: 1, label: "Nacional" },
+      { value: 2, label: "Internacional" },
+    ]
+  },
+  {
+    value: 30, label: 'Nacionalidade', type: 'select',
+    options: [
+      { value: 1, label: "Brasileira"},
+      { value: 2, label: "Brasileira - nascido no exterior ou naturalizado"},
+      { value: 3, label: "Estrangeira"},
+    ]
+  },
+];
 
-  {value: 9, label: 'Valor de alguma coisa', type: 'input'},
-
+export const collegeOptionsMock = [
+  {
+    value: 2, label: 'Grau Acadêmico', type: 'select',
+    options: [
+      { value: 1,	label: "Bacharelado" },
+      { value: 2,	label: "Licenciatura" },
+      { value: 3,	label: "Tecnológico" },
+      { value: 4,	label: "Bacharelado e Licenciatura" },
+    ]
+  },
+  {
+    value: 3, label: 'Turno', type: 'select',
+    options: [
+      { value: 1, label: "Matutino" },
+      { value: 2, label: "Vespertino" },
+      { value: 3, label: "Noturno" },
+      { value: 4, label: "Integral" },
+    ]
+  },
+  {
+    value: 4, label: 'Nível Acadêmico', type: 'select',
+    options: [
+      { value: 1, label: "Graduação" },
+      { value: 2, label:"Seqüencial de Formação Específica" },
+    ]
+  },
+  {
+    value: 5, label: 'Modalidade de Ensino', type: 'select',
+    options: [
+      { value: 1, label: "Presencial" },
+      { value: 2, label:"Curso a Distância" },
+    ]
+  },
 ];
 
 export const iesOptionsMock = [

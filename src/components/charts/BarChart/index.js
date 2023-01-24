a5f2cd0e3363e0500bf947f8ba54b39531098982
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-01-24 17:26:58 -0300

    Finish bar chart to work dinamicaly

--- src/components/charts/BarChart/index.js
+++ src/components/charts/BarChart/index.js
@@ -1,91 +1,58 @@
-// import "./styles.css";
-import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import {
   BarChart,
-  Bar,
+  CartesianGrid,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
-  Legend
+  Bar,
+  Cell
 } from "recharts";
-
+import { useSelector } from 'react-redux';
+import ResearchDescription from '../../ResearchDescription';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     fontFamily: "sans-serif",
     textAlign: "center"
-  },
+  }
 }));
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
 export default function BarChartComponent() {
   const classes = useStyles();
+
+  const { researchData } = useSelector((state) => {
+    return state.infographic;
+  });
+
   return (
-    <BarChart
-      classes={{ root: classes }}
-      width={700}
-      height={500}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
-    </BarChart>
+    <>
+      <BarChart
+        classes={{ root: classes }}
+        width={700}
+        height={500}
+        data={researchData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="ano">
+        </XAxis>
+        <YAxis label={{ value: 'quantidade total', angle: -90, position: 'insideLeft' }} />
+        <Tooltip />
+        <Bar dataKey="total">
+          {researchData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+      <ResearchDescription />
+    </>
   );
 }

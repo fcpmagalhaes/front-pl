731c6890fdc47be841411e21cebf9a2e2361eb65
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-01-25 20:36:13 -0300

    finish pie chart

--- src/components/charts/PieChart/index.js
+++ src/components/charts/PieChart/index.js
@@ -1,16 +1,7 @@
-// import "./styles.css";
-import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
-
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { useSelector } from 'react-redux';
+import ResearchDescription from '../../ResearchDescription';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,73 +10,43 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+const colors = ['#0088FE', '#00C49F', '#FF8042', '#FFBB28', 'red', 'pink'];
 
 export default function PieChartComponent() {
   const classes = useStyles();
+
+  const { researchData } = useSelector((state) => {
+    return state.infographic;
+  });
+
   return (
-    <BarChart
-      classes={{ root: classes }}
-      width={700}
-      height={500}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
-    </BarChart>
+    <>
+        <PieChart
+          classes={{ root: classes }}
+          width={700}
+          height={500}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 30
+          }}
+        >
+          <Pie
+            data={researchData}
+            outerRadius={200}
+            fill="#8884d8"
+            nameKey="ano"
+            dataKey="total"
+            label
+          >
+            {researchData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      <ResearchDescription />
+    </>
   );
 }

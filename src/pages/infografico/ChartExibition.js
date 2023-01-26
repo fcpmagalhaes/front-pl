9cacee6d1e5b87dacad24eae098b9744657bc7ae
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-01-26 00:44:54 -0300

    Fix bug

--- src/pages/infografico/ChartExibition.js
+++ src/pages/infografico/ChartExibition.js
@@ -1,4 +1,3 @@
-import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import BarChartComponent from '../../components/charts/BarChart';
@@ -6,17 +5,10 @@ import AreaChartComponent from '../../components/charts/AreaChart';
 import LineChartComponent from '../../components/charts/LineChart';
 import PieChartComponent from '../../components/charts/PieChart';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    fontFamily: "sans-serif",
-    textAlign: "center"
-  },
-}));
-
 function getChartContent(step) {
   switch (step) {
-    case 'area': 
-      return <AreaChartComponent />
+    case 'area':
+      return <AreaChartComponent />;
     case 'line':
       return <LineChartComponent />;
     case 'bar':
@@ -29,13 +21,16 @@ function getChartContent(step) {
 }
 
 function ChartExibition() {
-  const classes = useStyles();
-  const { chartType } = useSelector((state) => {
-    return state.infographic;
-  });
+  const { chartType } = useSelector((state) => state.infographic);
 
   return (
-    <Grid container direction="row" spacing={3} justifyContent="space-evenly" alignItems="center">
+    <Grid
+      container
+      direction="row"
+      spacing={3}
+      justifyContent="space-evenly"
+      alignItems="center"
+    >
       <Grid item md={6} xs={12}>
         {getChartContent(chartType)}
       </Grid>

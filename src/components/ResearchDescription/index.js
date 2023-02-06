8311dbd9e9449236a212827b345a96e9a7572272
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2023-02-06 02:42:29 -0300

    Change final description

--- src/components/ResearchDescription/index.js
+++ src/components/ResearchDescription/index.js
@@ -1,20 +1,18 @@
-
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   textDescription: {
     width: '100%',
     textAlign: 'center',
-    fontSize: '15px'
-  }
-}))
+    fontSize: '15px',
+  },
+}));
 
 const ResearchDescription = () => {
   const classes = useStyles();
-  const text = "Instituição: Universidade de Brasília e UCB curso: Computação, cor/raça: preta, parda, indígena, ingresso por vestibular.";
-  return (
-    <div className={classes.textDescription}>{text}</div>
-  )
-}
+  const text =
+    'Instituição: Universidade de Brasília, curso: Computação Licenciatura, situação: formado.';
+  return <div className={classes.textDescription}>{text}</div>;
+};
 
 export default ResearchDescription;
